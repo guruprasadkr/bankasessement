175d7ad0361c143c8fb558e99ca0564ea4c741a7
guruprasadkr <Guru.Prasad@standardbank.co.za>
2017-05-09 00:58:01 +0200

    “Designcompleted

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -1,13 +1,21 @@
 package com.wipro.bank.assessment;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wipro.bank.entity.Account;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static final List<Account> accounts =new ArrayList<Account>();
+	public Account getAccountDetailsByID(int id)
+	{
+		return null;
+		
+	}
+	public List<Account> getAccountDetailsByBalance(double amount)
+	{
+		return null;
+		
+	}
 }
